b83a90420867163d38ed4878d0007c2a206b6ec7
hugeblank <hugeblank@gmail.com>
2025-11-17 20:56:07 -0800

    fix(bouquet): make cactallium functional I still think this should be replaced with something more inspired since the game has cactus flowers now.

--- bouquet/examples/cactallium/mixin.lua
+++ bouquet/examples/cactallium/mixin.lua
@@ -1,13 +1,13 @@
 -- Let's add an allium on top of fully grown hand-planted cacti!
-local CactusBlockMixin = mixin.to("net.minecraft.block.CactusBlock") -- Mix into the block class
+local CactusBlockMixin = mixin.to("net.minecraft.world.level.block.CactusBlock") -- Mix into the block class
 addFlower = CactusBlockMixin:inject("add_flower", { -- Target the randomTick method, at a specific point after the cacti has grown
-    method = { "randomTick(Lnet/minecraft/block/BlockState;Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/random/Random;)V" },
+    method = { "randomTick(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/core/BlockPos;Lnet/minecraft/util/RandomSource;)V" },
     at = { {
             value = "INVOKE",
-            target = "Lnet/minecraft/server/world/ServerWorld;updateNeighbor(Lnet/minecraft/block/BlockState;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/Block;Lnet/minecraft/util/math/BlockPos;Z)V"
+            target = "Lnet/minecraft/server/level/ServerLevel;neighborChanged(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/Block;Lnet/minecraft/world/level/redstone/Orientation;Z)V"
         } }
 }, {
-    mixin.getLocal("Lnet/minecraft/util/math/BlockPos;", "ordinal", 0), -- Get this block pos, not necessary but it's nice to demonstrate that we can
+    mixin.getLocal("Lnet/minecraft/core/BlockPos;", "ordinal", 0), -- Get this block pos, not necessary but it's nice to demonstrate that we can
     mixin.getLocal("I", "ordinal", 0) -- Get the cactus growth level.
 })
 
