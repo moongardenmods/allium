11217e3993c9f033c5a460bb4ed6dbdd920649e1
hugeblank <hugeblank@gmail.com>
2025-12-13 22:58:31 -0800

    fix(allium): Add mixinextras meta to MixinConfigUtil enables expression annotations for script mixins :thumbs_up:

--- allium/src/main/java/dev/hugeblank/allium/loader/mixin/annotation/sugar/MixinSugars.java
+++ allium/src/main/java/dev/hugeblank/allium/loader/mixin/annotation/sugar/MixinSugars.java
@@ -16,18 +16,18 @@ public class MixinSugars {
     /// If `mutable` is set, the parameter becomes a corresponding LocalRef.
     /// @see com.llamalad7.mixinextras.sugar.ref
     @LuaWrapped
-    public static LuaLocal localref(@LuaStateArg LuaState state, String type, @OptionalArg @Nullable LuaTable annotationTable, @OptionalArg @Nullable Boolean mutable) throws InvalidArgumentException, LuaError {
+    public static LuaLocal localref(@LuaStateArg LuaState state, String type, @OptionalArg @Nullable LuaTable annotation, @OptionalArg @Nullable Boolean mutable) throws InvalidArgumentException, LuaError {
         return new LuaLocal(state, type,
                 mutable != null && mutable,
-                annotationTable == null ? new LuaTable() : annotationTable);
+                annotation == null ? new LuaTable() : annotation);
     }
 
     /// Add a shared value to the parameter list.
     /// Shared values are preserved across multiple injections on the same method.
     /// @see com.llamalad7.mixinextras.sugar.Share
     @LuaWrapped
-    public static LuaShare share(@LuaStateArg LuaState state, String type, LuaTable annotationTable) throws InvalidArgumentException, LuaError {
-        return new LuaShare(state, type, annotationTable);
+    public static LuaShare share(@LuaStateArg LuaState state, String type, LuaTable annotation) throws InvalidArgumentException, LuaError {
+        return new LuaShare(state, type, annotation);
     }
 
     /// Add a CallbackInfo or CallbackInfoReturnable to the list of parameters.
