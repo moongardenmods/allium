f36fea174c383984faf655b6c60437ab187505b8
hugeblank <hugeblank@gmail.com>
2025-12-15 23:09:05 -0800

    fix(allium)!: better method naming

--- allium/src/main/java/dev/hugeblank/allium/api/ScriptResource.java
+++ allium/src/main/java/dev/hugeblank/allium/api/ScriptResource.java
@@ -2,7 +2,7 @@ package dev.hugeblank.allium.api;
 
 /**
  * Temporary resource to be provided to a dynamic script.
- * On script reload, the <code>close()</code> method gets invoked.
+ * On script unload/reload, the <code>close()</code> method gets invoked.
  */
 public interface ScriptResource extends AutoCloseable {
     @Override
--- allium/src/main/java/dev/hugeblank/allium/loader/lib/AlliumLib.java
+++ allium/src/main/java/dev/hugeblank/allium/loader/lib/AlliumLib.java
@@ -1,14 +1,14 @@
 package dev.hugeblank.allium.loader.lib;
 
-import dev.hugeblank.allium.loader.Script;
 import dev.hugeblank.allium.api.WrappedLibrary;
+import dev.hugeblank.allium.loader.Script;
 import dev.hugeblank.allium.loader.ScriptRegistry;
 import dev.hugeblank.allium.loader.type.annotation.CoerceToNative;
 import dev.hugeblank.allium.loader.type.annotation.LuaWrapped;
 import net.fabricmc.loader.api.FabricLoader;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Set;
+import java.util.List;
 
 @LuaWrapped(name = "allium")
 public class AlliumLib implements WrappedLibrary {
@@ -22,18 +22,24 @@ public class AlliumLib implements WrappedLibrary {
     }
 
     @LuaWrapped
-    public boolean isScriptLoaded(String id) {
-        return ScriptRegistry.getInstance().has(id);
+    public boolean isScriptPresent(String id) {
+        return ScriptRegistry.getInstance().has(id) &&
+                ScriptRegistry.getInstance().get(id).getLaunchState().equals(Script.State.INITIALIZED);
     }
 
     @LuaWrapped
-    public @CoerceToNative Set<Script> getAllScripts() {
-        return ScriptRegistry.getInstance().getAll();
+    public @CoerceToNative List<Script> getAllScripts() {
+        return ScriptRegistry.getInstance().getAll().stream()
+                .filter((script) -> script.getLaunchState().equals(Script.State.INITIALIZED)).toList();
     }
 
     @LuaWrapped
     public @Nullable Script getScript(String id) {
-        return ScriptRegistry.getInstance().get(id);
+        Script instance = ScriptRegistry.getInstance().get(id);
+        if (instance != null && instance.getLaunchState().equals(Script.State.INITIALIZED)) {
+            return ScriptRegistry.getInstance().get(id);
+        }
+        return null;
     }
 
 }
