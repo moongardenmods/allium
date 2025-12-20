2d139fd81c21dae97e929399bc343a524996bafc
hugeblank <hugeblank@gmail.com>
2025-12-19 23:53:32 -0800

    feat(bouquet)!: Component library & LuaOps
    Replaces TextLib

--- bouquet/examples/hangman/main.lua
+++ bouquet/examples/hangman/main.lua
@@ -9,12 +9,25 @@ local words = require("words")
 local Commands = require("net.minecraft.commands.Commands") -- We need the java command manager for creating commands.
 local bouquet = require("bouquet")
 local arguments = bouquet.command.arguments -- Create shortcut for command argument types
-local fs = bouquet.fs.as(script)
+local component = bouquet.component(script)
+local fs = bouquet.fs(script)
 
 local active = {}
 
 local leaderboard = {}
 
+local shortcuts = {
+    space = { text = " " },
+    hangman_start = {
+        text = "/hangman start",
+        color = "gray",
+        click_event = { action = "suggest_command", command = "/hangman start" },
+        hover_event = { action = "show_text", value = "Click to suggest" }
+    }
+}
+
+-- unused helper function to test functions.
+-- when a command errors it does not output a stack trace, so we print the error by wrapping a function in this one.
 local function debugwrap(func)
     return function(...)
         local out = {pcall(func, ...)}
@@ -85,12 +98,77 @@ local function parseGuess(word, guessed)
     return out
 end
 
-local function sendMessage(context, data) -- easily broadcast a message to all players
-    local out = bouquet.text.format(data)
+local function sendMessage(context, data) -- broadcast a message to the player
+    local out = component.fromTable(data)
     context
             :getSource()
             :getPlayer()
             :sendSystemMessage(out)
+end
+
+local function sendWin(context, game, player) -- easily broadcast win message
+    sendMessage(context, {
+        text = "You guessed the word! It was: ",
+        color = "green",
+        extra = {
+            { text = table.concat(game.word), bold = true }
+        }
+    })
+    active[player] = nil
+    flush(player, true)
+    return 1
+end
+
+local function simpleComponent(text, color)
+    return { text = text, color = color }
+end
+
+local function getInfo(uuid)
+    local user = leaderboard[uuid]
+    local out = {
+        text = "",
+        extra = {
+            { text = user.username .. " - ", color = "white" },
+            {
+                text = tostring(user.wins),
+                color = "green",
+                hover_event = {
+                    action = "show_text",
+                    value = "Wins"
+                }
+            },
+            shortcuts.space,
+            {
+                text = tostring(user.losses),
+                color = "red",
+                hover_event = {
+                    action = "show_text",
+                    value = "Losses"
+                }
+            },
+            shortcuts.space,
+            {
+                text = tostring(math.floor(user.wins/(user.wins+user.losses)*100000)/1000).."%",
+                color = "yellow",
+                hover_event = {
+                    action = "show_text",
+                    value = "Win rate"
+                }
+            },
+        }
+    }
+    return out
+end
+
+local function getTopPlayers()
+    local lb = {}
+    for _, v in pairs(leaderboard) do
+        lb[#lb+1] = v
+    end
+    table.sort(lb, function(a, b)
+        return a.wins-a.losses > b.wins-b.losses
+    end)
+    return lb
 end
 
 local builder = Commands.literal("hangman") -- Create the builder for the hangman command
@@ -105,61 +183,87 @@ bouquet.events.server.commandRegister:register(script, function(_, name, success
 end)
 
 do
-    local t = {
-        "A hangman game written entirely in Lua using <url 'https://github.com/moongardenmods/allium'>",
-        "<hover 'Click to view on GitHub'><light_purple>All</light_purple><dark_purple>i</dark_purple>",
-        "<light_purple>um</light_purple> & <rainbow>Bouquet</rainbow></hover></url>. Run <gray>",
-        "<cmd '/hangman start'><hover 'Click to suggest'>/hangman start</hover></cmd></gray> to start, ",
-        "guess with <gray><cmd '/hangman guess'><hover 'Click to suggest'>/hangman guess <letter|word>",
-        "</hover></cmd></gray>. Check out the ",
-        "<url 'https://github.com/moongardenmods/allium/blob/main/bouquet/examples/hangman/main.lua'>",
-        "<hover 'Click to view on GitHub'><blue>source code</blue></hover></url>!"
+    local c = {
+        text = "",
+        extra = {
+            { text = "A hangman game written entirely in Lua using " },
+            {
+                text = "",
+                extra = {
+                    simpleComponent( "All", "light_purple"),
+                    simpleComponent( "i", "dark_purple"),
+                    simpleComponent( "um", "light_purple"),
+                    { text = " & " },
+                    simpleComponent("B", "red" ),
+                    simpleComponent("o", "gold" ),
+                    simpleComponent("u", "yellow" ),
+                    simpleComponent("q", "green" ),
+                    simpleComponent("u", "aqua" ),
+                    simpleComponent("e", "light_purple" ),
+                    simpleComponent("t", "dark_purple" ),
+                },
+                click_event = { action = "open_url", url = "https://github.com/moongardenmods/allium" },
+                hover_event = { action = "show_text", value = "Click to view on GitHub"}
+            },
+            { text = ". Run " },
+            shortcuts.hangman_start,
+            { text = " to start, guess with " },
+            {
+                text = "/hangman guess <letter|word>",
+                color = "gray",
+                click_event = { action = "suggest_command", command = "/hangman guess " },
+                hover_event = { action = "show_text", value = "Click to suggest" }
+            },
+            { text = ". Check out the " },
+            {
+                text = "source code",
+                color = "blue",
+                click_event = {
+                    action = "open_url",
+                    url = "https://github.com/moongardenmods/allium/blob/main/bouquet/examples/hangman/main.lua"
+                },
+                hover_event = { action = "show_text", value = "Click to view on GitHub"}
+            },
+            { text = "!" },
+        }
     }
-    local info = table.concat(t)
     builder:executes(function(context)
-        sendMessage(context, info)
+        sendMessage(context, c)
         return 1
     end)
-end
-
-local function getInfo(uuid)
-    local user = leaderboard[uuid]
-    return table.concat({ "<white>", user.username, " - </white><hover 'Wins'><green>", user.wins,
-                          "</green></hover> <hover 'Losses'><red>", user.losses,
-                          "</red></hover> <hover 'Win Rate'><yellow>",
-                          tostring(math.floor(user.wins/(user.wins+user.losses)*100000)/1000),
-                          "%</yellow></hover>"
-    })
-end
-
-local function getTopPlayers()
-    local lb = {}
-    for _, v in pairs(leaderboard) do
-        lb[#lb+1] = v
-    end
-    table.sort(lb, function(a, b)
-        return a.wins-a.losses > b.wins-b.losses
-    end)
-    return lb
 end
 
 builder:m_then(Commands.literal("leaderboard"):executes(function(context)
     local player = context:getSource():getPlayer()
     local uuid = player:getGameProfile():id():toString()
-    local out = { "Your stats:" }
+    local extra = {  }
+    local out = { text = "Your stats:\n", extra = extra }
     if leaderboard[uuid] then
-        out[#out+1] = getInfo(uuid)
+        extra[#extra+1] = {
+            text = "",
+            extra = {
+                getInfo(uuid),
+                { text = "\n" }
+            }
+        }
     else
-        out[#out+1] = "<red>No data!</red>"
+        extra[#extra+1] = { text = "No data!\n", color = "red" }
     end
-    out[#out+1] = "Leaderboard:"
+    extra[#extra+1] = { text = "Leaderboard:\n" }
     local lb = getTopPlayers()
     for i = 1, 10 do
         local record = lb[i]
         if not record then break end
-        out[#out+1] = i..". "..getInfo(record.uuid)
+        local add = { getInfo(record.uuid) }
+        if i < 10 then
+            add[#add+1] = { text = "\n" }
+        end
+        extra[#extra+1] = {
+            text = i..". ",
+            extra = add
+        }
     end
-    sendMessage(context, table.concat(out, "\n"))
+    sendMessage(context, out)
     return 1
 end))
 
@@ -181,24 +285,16 @@ builder:m_then(Commands.literal("start"):executes(function(context) -- The part 
             game.guessed[i] = false
         end
         active[player] = game
-        sendMessage(context, string.format(
-                "Guess the word!\n<bold>%s</bold>\nYou have %d guesses. Good luck!",
-                parseGuess(game.word, game.guessed),
-                game.guesses
-        ))
+        sendMessage(context, {
+            text = "Guess the word!\n",
+            extra = {
+                { text = parseGuess(game.word, game.guessed), bold = true },
+                { text = "\nYou have "..game.guesses.." guesses. Good luck!" }
+            }
+        })
         return 1
     end
 end))
-
-local function sendWin(context, game, player) -- easily broadcast win message
-    sendMessage(context, string.format(
-            "<green>You guessed the word! It was: <bold>%s</bold></green>",
-            table.concat(game.word)
-    ))
-    active[player] = nil
-    flush(player, true)
-    return 1
-end
 
 builder:m_then(Commands.literal("guess"):m_then(Commands.argument("guess", arguments.string.word()):executes(function(context)
     -- The part of the command that handles guesses
@@ -206,7 +302,12 @@ builder:m_then(Commands.literal("guess"):m_then(Commands.argument("guess", argum
     local game = active[player]
     local str = arguments.string.getString(context, "guess"):lower() -- Get the guess from the command context
     if game then -- If theres a game running
-        sendMessage(context, "You guessed <bold>"..str.."</bold>")
+        sendMessage(context, {
+            text = "You guessed ",
+            extra = {
+                { text = str, bold = true }
+            }
+        })
         if #str == 1 then -- If the guess is a letter
             local correct = false
             local total = 0 -- Keep track of the total number of letters guessed so far
@@ -220,34 +321,48 @@ builder:m_then(Commands.literal("guess"):m_then(Commands.argument("guess", argum
             if total == #game.word then -- If all letters have been guessed
                 return sendWin(context, game, player)
             elseif correct then -- If the guess was marked as correct
-                sendMessage(context, "<green>You guessed a letter correctly!</green>")
+                sendMessage(context, simpleComponent("You guessed a letter correctly!", "green"))
             else
-                sendMessage(context, "<red>You guessed a letter incorrectly!</red>")
+                sendMessage(context, simpleComponent("You guessed a letter incorrectly!", "red"))
                 game.guesses = game.guesses-1 -- Subtract a guess
             end
         else -- If the guess is a word
             if str == table.concat(game.word) then -- If the guessed word is an exact match
                 return sendWin(context, game, player)
             else -- Otherwise the guess is incorrect
-                sendMessage(context, "<red>You guessed incorrectly! </red>")
+                sendMessage(context, simpleComponent("You guessed incorrectly!", "red"))
                 game.guesses = game.guesses-1 -- Subtract a guess
             end
         end
         if game.guesses > 0 then -- So long as the game has guesses left
-            sendMessage(context, "<bold>"..parseGuess(game.word, game.guessed).."</bold>")
             local s = " guesses"
             if game.guesses == 1 then s = " guess" end -- Handle the English language
-            sendMessage(context, tostring(game.guesses)..s.." left")
+            sendMessage(context, { text = "", extra = {
+                { text = parseGuess(game.word, game.guessed), bold = true },
+                { text = "\n"..tostring(game.guesses)..s.." left" }
+            } })
         else -- No guesses left, game over!
-            sendMessage(context, string.format(
-                    "<red><bold>Game over!</bold> The word was: <bold>%s</bold></red>",
-                    table.concat(game.word)
-            ))
+            sendMessage(context, {
+                text = "",
+                color = "red",
+                extra = {
+                    { text = "Game over!", bold = true },
+                    { text = "The word was: " },
+                    { text = table.concat(game.word) }
+                }
+            })
             active[player] = nil
             flush(player, false)
         end
     else -- No game, tell player how to start one
-        context:getSource():sendFailure(bouquet.text.format("No game! <gray>Use /hangman start</gray> to play!"))
+        context:getSource():sendFailure(component.fromTable({
+            text = "No game! ",
+            extra ={
+                { text = "Use " },
+                shortcuts.hangman_start,
+                { text = " to play!" }
+            }
+        }))
     end
     return 1
 end)
---
+++ bouquet/src/main/resources/bouquet/api/component.lua
@@ -0,0 +1,34 @@
+local Script = require("dev.hugeblank.allium.loader.Script")
+local ComponentSerialization = require("net.minecraft.network.chat.ComponentSerialization")
+local LuaOps = require("dev.hugeblank.bouquet.util.LuaOps")
+local LuaValue = require("org.squiddev.cobalt.LuaValue")
+local Component = require("net.minecraft.network.chat.Component")
+
+local util = require("bouquet.util")
+
+local function testResult(result)
+    if result:isError() and result:error():isPresent() then
+        error(result:error():get():message())
+    elseif result:isError() then
+        error("An unknown error occurred during parsing.")
+    end
+    return result:getOrThrow()
+end
+
+-- Parses components to and from a table.
+-- For information on how to format a component table, see: https://minecraft.wiki/w/Text_component_format#Content_types
+
+return function(script)
+    util.assertType(1, script, Script)
+    local ops = LuaOps(script:getState())
+    return {
+        fromTable = function(data)
+            assert(type(data) == "table", "Argument #1: Expected type table, got "..type(data))
+            return testResult(java.callWith(ComponentSerialization.CODEC.parse, {LuaOps, LuaValue}, ComponentSerialization.CODEC, ops, data))
+        end,
+        toTable = function(component)
+            util.assertType(1, component, Component)
+            return testResult(java.callWith(ComponentSerialization.CODEC.encodeStart, {LuaOps, Component}, ComponentSerialization.CODEC, ops, component))
+        end
+    }
+end
