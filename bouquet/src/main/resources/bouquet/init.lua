f903fae599d9baa4a82dac5403e16dfd926f1b3b
hugeblank <hugeblank@gmail.com>
2025-12-19 23:53:32 -0800

    feat(bouquet)!: Component library & LuaOps
    Replaces TextLib

--- bouquet/src/main/resources/bouquet/init.lua
+++ bouquet/src/main/resources/bouquet/init.lua
@@ -3,26 +3,29 @@ require("bouquet.hooks")
 local FsLib = require("dev.hugeblank.bouquet.api.lib.fs.FsLib")
 local HttpLib = require("dev.hugeblank.bouquet.api.lib.http.HttpLib")
 
+local function wrapAs(class)
+    return function(script)
+        local lib = class(script)
+        local out = {}
+        for k, v in pairs(lib) do
+            out[k] = function(...)
+                return v(lib, ...)
+            end
+        end
+        return out
+    end
+end
+
 print("Hello from Bouquet's Script! Environment:", allium.environment())
 local request = HttpLib().request
 return {
     command = require("bouquet.api.command"),
     commands = require("bouquet.api.commands"),
+    component = require("bouquet.api.component"),
     config = require("bouquet.api.config"),
     events = require("bouquet.api.events"),
     fabric = require("bouquet.api.fabric"),
-    fs = {
-        as = function(script)
-            local ofs = FsLib(script)
-            local fs = {}
-            for k, v in pairs(ofs) do
-                fs[k] = function(...)
-                    return v(ofs, ...)
-                end
-            end
-            return fs
-        end
-    },
+    fs = wrapAs(FsLib),
     game = require("bouquet.api.game"),
     http = {
         request = request
