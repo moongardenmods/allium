342fc6ac1cbce308ee3787ee1aa2c80c59c114b7
BasiqueEvangelist <basiqueevangelist@yandex.ru>
2025-12-24 07:08:06 +0300

    make the http library use java's (new?) http client api (#51)
    * make the http library use java's (new?) http client api
    
    also fix bouquet init so that http actually works
    
    * chore: remove netty gradle config
    
    Properly removed the dependency a while ago but forgot to remove the rest. Committing the changes here for completeness’s sake.
    
    ---------
    
    Co-authored-by: hugeblank <hugeblank@gmail.com>

---
+++ bouquet/src/main/java/dev/hugeblank/bouquet/api/lib/http/LazyBodyPublisher.java
@@ -0,0 +1,26 @@
+package dev.hugeblank.bouquet.api.lib.http;
+
+import com.google.common.base.Suppliers;
+
+import java.net.http.HttpRequest;
+import java.nio.ByteBuffer;
+import java.util.concurrent.Flow;
+import java.util.function.Supplier;
+
+public class LazyBodyPublisher implements HttpRequest.BodyPublisher {
+    private final Supplier<HttpRequest.BodyPublisher> factory;
+
+    public LazyBodyPublisher(Supplier<HttpRequest.BodyPublisher> factory) {
+        this.factory = Suppliers.memoize(factory::get);
+    }
+
+    @Override
+    public long contentLength() {
+        return factory.get().contentLength();
+    }
+
+    @Override
+    public void subscribe(Flow.Subscriber<? super ByteBuffer> subscriber) {
+        factory.get().subscribe(subscriber);
+    }
+}
---
+++ bouquet/src/main/java/dev/hugeblank/bouquet/api/lib/http/LuaHttpResponse.java
@@ -0,0 +1,33 @@
+package dev.hugeblank.bouquet.api.lib.http;
+
+import dev.hugeblank.allium.loader.type.annotation.CoerceToNative;
+import dev.hugeblank.allium.loader.type.annotation.LuaWrapped;
+import io.netty.buffer.Unpooled;
+
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
+
+@LuaWrapped
+public class LuaHttpResponse {
+    private final HttpResponse<byte[]> raw;
+    @LuaWrapped public final LuaByteBuf body;
+
+    public LuaHttpResponse(HttpResponse<byte[]> raw) {
+        this.raw = raw;
+
+        var buf = Unpooled.wrappedBuffer(raw.body());
+        this.body = new LuaByteBuf(buf, StandardCharsets.UTF_8); // TODO: figure out what charset to use
+    }
+
+    @LuaWrapped
+    public int status() {
+        return raw.statusCode();
+    }
+
+    @LuaWrapped
+    public @CoerceToNative Map<String, @CoerceToNative List<String>> headers() {
+        return raw.headers().map();
+    }
+}
