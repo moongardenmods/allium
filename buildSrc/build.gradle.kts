5d65c4cdb442abc61a01c3282dd58b71953566eb
hugeblank <hugeblank@gmail.com>
2025-12-25 05:21:53 -0800

    chore: move build locations & buildSrc properties

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,6 +1,15 @@
+import org.jetbrains.kotlin.konan.properties.Properties
+import kotlin.io.path.inputStream
+
 plugins {
     `kotlin-dsl`
 }
+
+val props: Properties = Properties()
+props.load(rootDir.toPath().resolve("../gradle.properties").inputStream())
+props.forEach { project.ext[it.key.toString()] = it.value }
+
+layout.buildDirectory = file("../build/buildSrc")
 
 dependencies {
     implementation("net.fabricmc:fabric-loom:${project.properties["loomVersion"]}")
