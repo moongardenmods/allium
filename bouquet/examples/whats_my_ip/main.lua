b6467f78c149d538fe8fb45362a87f42be557111
Basique <basiqueevangelist@yandex.ru>
2026-01-02 22:30:42 +0300

    add 'What's my IP?' example

---
+++ bouquet/examples/whats_my_ip/main.lua
@@ -0,0 +1,21 @@
+-- What's my IP?
+-- By BasiqueEvangelist - Jan 02, 2026
+-- A simple script to demonstrate the HTTP API in Bouquet.
+-- Registers /whats_my_ip, which shows your public IP address when run.
+
+local bouquet = require("bouquet")
+local Commands = require("net.minecraft.commands.Commands")
+local Component = require("net.minecraft.network.chat.Component")
+
+bouquet.command.register(script, Commands.literal("whats_my_ip"):executes(function(context)
+    local req = bouquet.http:request("https://httpbin.org/get")
+    local json = req:send():thenAccept(function(res)
+        local json = res.body:asJson()
+        print(json)
+        context:getSource():sendSuccess(function()
+            return Component.literal(json.origin)
+        end, false)
+    end)
+
+    return 0
+end))
